572b31686cd771d030e08568126d658fb741c6c4
haris <135050632+Harisshafi01@users.noreply.github.com>
2023-11-13 15:39:56 +0530

    Add files via upload

---
+++ number guessing game 18/number guessing game 18/Program.cs
@@ -0,0 +1,51 @@
+﻿Random random = new Random();
+bool playagain = true;
+int min = 1;
+int max = 100;
+int guess;
+int number;
+int guesses;
+string response;
+
+while (playagain)
+{
+    guess = 0;
+    guesses = 0;
+    number = random.Next(min, max + 1);
+    response = "";
+
+    while (guess != number)
+    {
+        Console.WriteLine("Guess the number between " + min + " - " + max + " :");
+        guess = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("guess: " + guess);
+
+        if (guess > number)
+        {
+            Console.WriteLine(guess + " is too high");
+        }
+        else if (guess < number)
+        {
+            Console.WriteLine(guess + " is too low");
+        }
+        guesses++;
+    }
+    Console.WriteLine("number: " + number);
+    Console.WriteLine("YOU WON!");
+    Console.WriteLine("guesses : " + guesses);
+
+    Console.WriteLine("Would you like to play again (Y/N) :");
+    response = Console.ReadLine();
+    response = response.ToUpper();
+
+    if (response == "Y")
+    {
+        playagain = true;
+    }
+    else
+    {
+        playagain = false;
+    }
+    Console.WriteLine("thanks for playing");
+}
+Console.ReadKey();
